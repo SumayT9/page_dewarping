e6e07f4fbcb2d6d0e154264c3390e724dc535b0e
SumayT9 <sthak99@gmail.com>
2021-02-28 15:41:35 -0800

    Added skeletons of methods that have to be implemented

---
+++ dewarp.py
@@ -0,0 +1,28 @@
+"""
+This is an implementation of the paper by Fu et. al "A Model-based Book Dewarping Method Using Text Line Detection"
+"""
+
+# 3.1.1
+def text_line_enhancement(image):
+    pass
+
+
+# 3.1.2
+def pixel_classification(image):
+    pass
+
+
+# 3.2.1
+def left_right_boundary_estimation(image):
+    pass
+
+
+# 3.2.2
+def top_bottom_curves_selection(image):
+    pass
+
+
+# 3.3
+def rectify_document(image):
+    pass
+
